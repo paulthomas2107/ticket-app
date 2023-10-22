d335018f827421f8d0d88cefe9966fbc81ffc57f
Paul Thomas <paulthomas2107@gmail.com>
2023-10-22 17:44:51 +0100

    More

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -12,19 +12,17 @@ const TicketCard = ({ ticket }) => {
           <DeleteBlock />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2 " />
-      <p className="whitespace-pre-wrap">
-        This is a ticket. Please do this ticket !
-      </p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">08/31/22 10:43PM</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
